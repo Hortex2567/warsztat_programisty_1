f5c0e500bee5e3026b8a0a8c2794beacab4cd7d8
Bartosz Bialas <z1143721@s0053-11.stud.matinf.uj.edu.pl>
2017-10-26 09:29:16 +0200

    Added bubble sort

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@ using namespace std;
 
 int max(int* tab, int length);
 int min(int* tab, int length);
+void bubble_sort(int* tab, int length);
+void show_table(int* tab, int length);
 
 int main() {
     int length = 10;
     int tab[length];
-    for (int i = 0; i < length; i++) tab[i] = rand() * 100;
+    for (int i = 0; i < length; i++) tab[i] = rand() % 100;
+    for (int i = 0; i < length; i++) cout << tab[i] << ", ";
+    cout << endl << endl;
+    // cout << "min, max: " << min(tab, 13) << ", " << max(tab, 13) << endl;
+    bubble_sort(tab, 10);
+    cout << endl;
     for (int i = 0; i < length; i++) cout << tab[i] << ", ";
     cout << endl;
-    cout << "min, max: " << min(tab, 13) << ", " << max(tab, 13) << endl;
+
     return 0;
 }
 
@@ -30,4 +37,25 @@ int min(int* tab, int length) {
         if (current > tab[i]) current = tab[i];
     }
     return current;
+}
+
+void bubble_sort(int* tab, int length) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length - i -1; j++) {
+            show_table(tab, length);
+            if (tab[j] > tab[j+1]) {
+                int temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void show_table(int* tab, int length) {
+    for (int i = 0; i < length; i++) {
+        cout << tab[i];
+        if (i != length - 1) cout << ", ";
+    }
+    cout << endl;
 }
